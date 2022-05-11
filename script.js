2d886592c6f9fd8fc45c77b148a6767c98c0c02f
Ryan Headlee <headleeryan@gmail.com>
2022-05-11 15:52:40 -0700

    Add checker for '#' in input.value and delete

--- script.js
+++ script.js
@@ -105,6 +105,8 @@ submit.addEventListener('click', () => {
     return submit.value = "Enter";
   }
 
+  if (input.value.substr(0, 1) === "#") input.value = input.value.slice(1, input.value.length);
+
   if (/^[a-fA-F\d]{3,6}$/.test(input.value) == false) return;
 
   colors.forEach(color => color.classList.remove("active"));
